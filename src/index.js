cc63f86fc4e7493a32da2731a64be7b1114430a5
Samuel Façanha <samufacanha@gmail.com>
2024-05-21 17:49:34 -0300

    feat: added conditional to check loaded env

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import { replaceTranslations } from "./utils/replaceTranslations.js";
 const oldText = process.env.OLD_TEXT;
 const newText = process.env.NEW_TEXT;
 
+console.log(
+  `Replacing "${oldText}" with "${newText}", do you want to continue? (y/n)`
+);
+const answer = await new Promise((resolve) => {
+  process.stdin.once("data", (data) => resolve(data.toString().trim()));
+});
+
+if (answer.toLowerCase() !== "y" && answer.toLowerCase() !== "yes") {
+  console.log("Aborted.");
+  process.exit(0);
+}
+
+console.log("Fetching translations...");
+
 await fetchTranslations(oldText);
 
 await replaceTranslations(oldText, newText);
